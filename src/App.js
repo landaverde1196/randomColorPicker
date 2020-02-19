f634ae330dc538b8686d1627c86a15ab8f612f19
landaverde1196 <alejndro1196@gmail.com>
2020-02-18 20:50:39 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./Button.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Random extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: [92, 132, 153]
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return "rgb(" + ary.join(", ") + ")";
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a, b) => a + b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random() * 256));
+    }
+    return random;
+  }
+
+  handleClick() {
+    this.setState({
+      color: this.chooseColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? "white" : "black"}>
+          Your color is {this.formatColor(this.state.color)}.
+        </h1>
+        <Button light={this.isLight()} onClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Random;
